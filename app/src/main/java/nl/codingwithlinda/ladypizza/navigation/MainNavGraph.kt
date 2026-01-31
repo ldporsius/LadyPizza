102f6c54e30f5062d8525475b0cc8ffdcc92998f
ldporsius <linda.d.porsius@gmail.com>
2026-01-31 10:58:44 +0100

    di

--- app/src/main/java/nl/codingwithlinda/ladypizza/navigation/MainNavGraph.kt
+++ app/src/main/java/nl/codingwithlinda/ladypizza/navigation/MainNavGraph.kt
@@ -5,7 +5,9 @@ import androidx.compose.ui.Modifier
 import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.ui.NavDisplay
+import nl.codingwithlinda.ladypizza.application.LadyPizzaApplication
 import nl.codingwithlinda.ladypizza.features.menu.presentation.MenuListScreen
+import nl.codingwithlinda.ladypizza.features.product_detail.presentation.ProductDetailRoot
 import nl.codingwithlinda.ladypizza.features.product_detail.presentation.ProductDetailScreen
 
 @Composable
@@ -13,6 +15,7 @@ fun MainNavGraph(modifier: Modifier = Modifier) {
 
     val backStack = rememberNavBackStack(Menu)
 
+    val extraToppingsViewModel = LadyPizzaApplication.appModule.extraToppingsViewModel
 
     NavDisplay(
         backStack = backStack,
@@ -22,8 +25,9 @@ fun MainNavGraph(modifier: Modifier = Modifier) {
                     is Menu -> MenuListScreen(
                         navToDetail = {id -> backStack.add(ProductDetail(id))}
                     )
-                    is ProductDetail -> ProductDetailScreen(
+                    is ProductDetail -> ProductDetailRoot(
                         key.id,
+                        extraToppingsViewModel = extraToppingsViewModel,
                         navBack = {backStack.retainAll(listOf(Menu))}
                     )
                 }
